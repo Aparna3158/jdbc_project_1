fa6aef65cfa159a2a3f227899def6239f8a75049
Polishetti Aparna <aaparna098@gmail.com>
2025-05-06 09:34:03 +0530

    Update HELLO.java

--- src/com/TEST/HELLO.java
+++ src/com/TEST/HELLO.java
@@ -3,8 +3,13 @@ package com.TEST;
 public class HELLO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		Scanner s = new Scanner(System.in);
+		System.out.println("enter a number ");
+		int number = scanner.nextInt();
+                int square = number * number;
+                System.out.println("The square is: " + square);
 
 	}
 
-}
+} 
